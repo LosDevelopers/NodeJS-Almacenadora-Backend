8a7da6db6dc72c6d9da0c884ae4b10eb8405ddbc
AngelJTumG <javierangel43.lo@gmail.com>
2025-04-16 12:16:16 -0600

    add: se añade el proyecto

---
+++ configs/swagger.js
@@ -0,0 +1,28 @@
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+const options ={
+    swaggerDefinition:{
+        openapi:"3.0.0",
+        info:{
+            title: "Almacenadora API",
+            version: "1.0.0",
+            description: "API para gestion de una almacenadora",
+            contact:{
+                name: "Developers",
+                email: "developersalm@gmail.com"
+            }
+        },
+        servers:[
+            {
+                url: "http://127.0.0.1:3000/Almacenadora/v1"
+            }
+        ]
+    },
+    apis:[
+    ]
+}
+
+const swaggerDocs = swaggerJSDoc(options)
+
+export { swaggerDocs, swaggerUi}
